fa051384f7e436bbfc42c1c8003ab638890ff966
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2024-05-15 14:22:06 +0200

    clnrod-testrule: clear cache first

--- src/rpc.rs
+++ src/rpc.rs
@@ -65,6 +65,7 @@ pub async fn clnrod_testrule(
                 let rule = r
                     .as_str()
                     .ok_or_else(|| anyhow!("rule: not a valid string"))?;
+                plugin.state().peerdata_cache.lock().remove(&pubkey);
                 let data = collect_data(
                     &plugin,
                     pubkey,
